631f4990c02b3f2ddae2ee89d16835c567d8da92
ThanadolU <dolthanadolu@gmail.com>
2025-01-05 14:52:00 +0700

    moved login and signup from components folder to pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import SignUp from './components/signup/SignUp'
+import SignUp from './pages/signup/SignUp'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
