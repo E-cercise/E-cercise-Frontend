01987b02721a04e5857318d0fe45b82ba2fff27c
ThanadolU <dolthanadolu@gmail.com>
2025-01-05 15:10:24 +0700

    created layout of login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import SignUp from './pages/signup/SignUp'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/login/Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <SignUp />
+      <Login />
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -7,16 +7,16 @@ function SignUp() {
         <div className='w-[27vw] h-[85vh] bg-[#E7E7E7] p-6 rounded-md'>
             <p className='text-center text-lg font-bold mb-5'>Sign Up</p>
             <div className='flex flex-col items-center space-y-5 mb-5'>                    
-                <AutoComplete placeholder="First Name" style={{ height: 30 }} className='w-full'/>
-                <AutoComplete placeholder="Last Name" style={{ height: 30 }} className='w-full'/>
-                <AutoComplete placeholder="Email" style={{ height: 30 }} className='w-full'/>
-                <AutoComplete placeholder="Password" style={{ height: 30 }} className='w-full'/>
-                <AutoComplete placeholder="Confirm Password" style={{ height: 30 }} className='w-full'/>
-                <AutoComplete placeholder="Address" style={{ height: 30 }} className='w-full'/>
-                <Divider style={{ borderColor: 'black' }} className='ml-2 mr-2'>Or with</Divider>
+                <AutoComplete placeholder='First Name' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='Last Name' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='Email' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='Password' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='Confirm Password' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='Address' style={{ height: 30 }} className='w-full'/>
+                <Divider style={{ borderColor: 'black', fontSize: 12 }}>Or with</Divider>
                 <Button className='w-full'>Sign Up</Button>
             </div>
-            <p className='text-center text-xs'>Already have an account? <a href="" className='underline underline-offset-1 text-[#0087E7]'>Login</a></p>
+            <p className='text-center text-xs'>Already have an account? <a href='' className='underline underline-offset-1 text-[#0087E7]'>Login</a></p>
         </div>
     </div>
   )
