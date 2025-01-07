820545df271c5f23b69e90db18fb19041e02c985
ThanadolU <dolthanadolu@gmail.com>
2025-01-07 09:59:07 +0700

    adjust placeholder font size and size of parent's div

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { AutoComplete, Button, Divider } from 'antd'
 import { Link } from 'react-router-dom'
+import './Login.css'
 
 function Login() {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-        <div className='w-[27vw] h-[52vh] bg-[#E7E7E7] p-6 rounded-md'>
-            <p className='text-center text-lg font-bold mt-2 mb-5'>Login</p>
+        <div className='w-[22vw] h-[330px] bg-[#E7E7E7] p-6 rounded-md'>
+            <p className='text-center text-lg font-bold mt-2 mb-6'>Login</p>
             <div className='flex flex-col items-center space-y-5 mb-5'>
                 <AutoComplete placeholder='Email' style={{ height: 30 }} className='w-full'/>
                 <AutoComplete placeholder='Password' style={{ height: 30 }} className='w-full' />
                 <Divider style={{ borderColor: 'black', fontSize: 12 }}>Or With</Divider>
-                <Button className='w-full'>Login</Button>
+                <Button className='w-full text-[12px] font-bold' color="default" variant="solid">Login</Button>
             </div>
             <p className='text-center text-xs '>Don't have an account? <Link to='/signup' className='underline underline-offset-1 text-[#0087E7]'>Sign Up</Link></p>
         </div>
--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { AutoComplete, Button, Divider } from 'antd'
 import { Link } from 'react-router-dom'
+import './SignUp.css'
 
 function SignUp() {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-        <div className='w-[27vw] h-[85vh] bg-[#E7E7E7] p-6 rounded-md'>
-            <p className='text-center text-lg font-bold mt-2 mb-5'>Sign Up</p>
+        <div className='w-[22vw] h-[530px] bg-[#E7E7E7] p-6 rounded-md'>
+            <p className='text-center text-lg font-bold mt-2 mb-6'>Sign Up</p>
             <div className='flex flex-col items-center space-y-5 mb-5'>                    
-                <AutoComplete placeholder='First Name' style={{ height: 30 }} className='w-full'/>
+                <AutoComplete placeholder='First Name' style={{ height: 30 }} className='w-full placeholder:text-sm'/>
                 <AutoComplete placeholder='Last Name' style={{ height: 30 }} className='w-full'/>
                 <AutoComplete placeholder='Email' style={{ height: 30 }} className='w-full'/>
                 <AutoComplete placeholder='Password' style={{ height: 30 }} className='w-full'/>
                 <AutoComplete placeholder='Confirm Password' style={{ height: 30 }} className='w-full'/>
                 <AutoComplete placeholder='Address' style={{ height: 30 }} className='w-full'/>
                 <Divider style={{ borderColor: 'black', fontSize: 12 }}>Or with</Divider>
-                <Button className='w-full'>Sign Up</Button>
+                <Button className='w-full text-[12px] font-bold' color="default" variant="solid">Sign Up</Button>
             </div>
             <p className='text-center text-xs'>Already have an account? <Link to='/login' className='underline underline-offset-1 text-[#0087E7]'>Login</Link></p>
         </div>
