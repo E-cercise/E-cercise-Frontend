6d39aec65a25f1b1274898a982b82eb640d5e00c
ThanadolU <dolthanadolu@gmail.com>
2025-02-15 00:07:09 +0700

    show the warning message on the interface when user forget to fill the inputs, or fill the wrong email that does exists

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Divider, Input } from "antd";
+import { Button, Divider, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { signUp } from "../../api/SignUp";
 import "./SignUp.css";
@@ -11,6 +11,10 @@ function SignUp() {
   const [password, setPassword] = useState<string>("");
   const [address, setAddress] = useState<string>("");
   const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [
+    showEmailAlreadyExistsMessage,
+    setShowEmailAlreadyExistsMessage,
+  ] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const handleOnChangeFirstName = (e: string) => {
@@ -60,78 +64,125 @@ function SignUp() {
       console.log(messageObject);
     } catch (err) {
       console.error(err);
+      setShowEmailAlreadyExistsMessage(true);
       navigate("/signup")
     }
   };
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="w-[280px] h-[530px] bg-[#E7E7E7] p-6 rounded-md">
+      <div className={`w-[280px] ${showEmailAlreadyExistsMessage ? "h-[590px]": "h-[560px]"} bg-[#E7E7E7] p-6 rounded-md`}>
         <p className="text-center text-lg font-bold mt-2 mb-6">Sign Up</p>
         <div className="flex flex-col items-center space-y-5 mb-5">
-          <Input
-            placeholder="First Name"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangeFirstName(e.target.value)}
-            value={firstName}
-          />
-          <Input
-            placeholder="Last Name"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangeLastName(e.target.value)}
-            value={lastName}
-          />
-          <Input
-            placeholder="Email"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangeEmail(e.target.value)}
-            value={email}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangePassword(e.target.value)}
-            value={password}
-          />
-          <Input
-            placeholder="Address"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangeAddress(e.target.value)}
-            value={address}
-          />
-          <Input
-            placeholder="Phone Number"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangePhoneNumber(e.target.value)}
-            value={phoneNumber}
-          />
-          <Divider style={{ borderColor: "black", fontSize: 12 }}>
-            Or with
-          </Divider>
-          <Button
-            className="w-full text-[12px] font-bold"
-            color="default"
-            variant="solid"
-            onClick={async () =>
-              await signUpUser(
-                firstName,
-                lastName,
-                email,
-                password,
-                address,
-                phoneNumber
-              )
+          <Form className="w-full">
+            <Form.Item
+              name="firstName"
+              rules={[{ required: true, message: "Please input your first name!" }]}
+            >
+              <Input
+                placeholder="First Name"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangeFirstName(e.target.value)}
+                value={firstName}
+              />
+            </Form.Item>
+            <Form.Item
+              name="lastName"
+              rules={[{ required: true, message: "Please input your last name!" }]}
+            >
+              <Input
+                placeholder="Last Name"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangeLastName(e.target.value)}
+                value={lastName}
+              />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input
+                placeholder="Email"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangeEmail(e.target.value)}
+                value={email}
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: "Please input your password!" }]}
+            >
+              <Input
+                placeholder="Password"
+                type="password"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangePassword(e.target.value)}
+                value={password}
+              />
+            </Form.Item>
+            <Form.Item
+              name="address"
+              rules={[{ required: true, message: "Please input your address!" }]}
+            >
+              <Input
+                placeholder="Address"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangeAddress(e.target.value)}
+                value={address}
+              />
+            </Form.Item>
+            <Form.Item
+              name="phoneNumber"
+              rules={[{ required: true, message: "Please input your phone number!" }]}
+            >
+              <Input
+                placeholder="Phone Number"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowEmailAlreadyExistsMessage(false)}
+                onChange={(e) => handleOnChangePhoneNumber(e.target.value)}
+                value={phoneNumber}
+              />
+            </Form.Item>
+            {
+              showEmailAlreadyExistsMessage && 
+              <div className="text-red-500">
+                * Email already exists
+              </div>
             }
-          >
-            Sign Up
-          </Button>
+            <Divider style={{ borderColor: "black", fontSize: 12 }}>
+              Or with
+            </Divider>
+            <Button
+              className="w-full text-[12px] font-bold"
+              color="default"
+              variant="solid"
+              onClick={async () => {
+                  await signUpUser(
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    address,
+                    phoneNumber
+                  )
+                }
+              }
+              htmlType="submit"
+            >
+              Sign Up
+            </Button>
+          </Form>
         </div>
         <p className="text-center text-xs">
           Already have an account?{" "}
