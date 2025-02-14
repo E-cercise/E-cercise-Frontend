049b578383c11913c40e7dbba2bc4357cf499a60
ThanadolU <dolthanadolu@gmail.com>
2025-02-14 23:18:14 +0700

    show the warning message on the interface when user forget to fill email or password, or fill the wrong email or password

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Divider, Input } from "antd";
+import { Button, Divider, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../../api/Login";
 // import CryptoJS from "crypto-js";
@@ -24,6 +24,10 @@ if (!ENCRYPT_SECRET_KEY) {
 function Login() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [
+    showIncorrectUsernamePasswordMessage,
+    setShowIncorrectUsernamePasswordMessage,
+  ] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const handleOnChangeEmail = (e: string) => {
@@ -65,51 +69,74 @@ function Login() {
       navigate(decoded.role === "USER" ? "/home" : "");
     } catch (err) {
       console.error(err);
+      setShowIncorrectUsernamePasswordMessage(true);
       navigate("/login");
     }
   };
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="w-[280px] h-[330px] bg-[#E7E7E7] p-6 rounded-md">
+      <div className={`w-[280px] ${showIncorrectUsernamePasswordMessage ? "h-[60vh]": "h-[54vh]"} bg-[#E7E7E7] p-6 rounded-md`}>
         <p className="text-center text-lg font-bold mt-2 mb-6">Login</p>
         <div className="flex flex-col items-center space-y-5 mb-5">
-          <Input
-            placeholder="Email"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangeEmail(e.target.value)}
-            value={email}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            style={{ height: 30 }}
-            className="w-full"
-            onChange={(e) => handleOnChangePassword(e.target.value)}
-            value={password}
-          />
-          <Divider style={{ borderColor: "black", fontSize: 12 }}>
-            Or With
-          </Divider>
-          <Button
-            className="w-full text-[12px] font-bold"
-            color="default"
-            variant="solid"
-            onClick={async () => await receivedToken(email, password)}
-          >
-            Login
-          </Button>
+          <Form className="w-full">
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input
+                placeholder="Email"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowIncorrectUsernamePasswordMessage(false)}
+                onChange={(e) => handleOnChangeEmail(e.target.value)}
+                value={email}
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input
+                placeholder="Password"
+                type="password"
+                style={{ height: 30 }}
+                className="w-full"
+                onClick={() => setShowIncorrectUsernamePasswordMessage(false)}
+                onChange={(e) => handleOnChangePassword(e.target.value)}
+                value={password}
+              />
+            </Form.Item>
+            {showIncorrectUsernamePasswordMessage && (
+              <div className="text-red-500">* Incorrect username or password</div>
+            )}
+            <Divider style={{ borderColor: "black", fontSize: 12 }}>
+              Or With
+            </Divider>
+            <Form.Item>
+              <Button
+                className="w-full text-[12px] font-bold"
+                color="default"
+                variant="solid"
+                onClick={async () => await receivedToken(email, password)}
+                htmlType="submit"
+              >
+                Login
+              </Button>
+            </Form.Item>
+            <p className="text-center text-xs ">
+              Don't have an account?{" "}
+              <Link
+                to="/signup"
+                className="underline underline-offset-1 text-[#0087E7]"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </Form>
         </div>
-        <p className="text-center text-xs ">
-          Don't have an account?{" "}
-          <Link
-            to="/signup"
-            className="underline underline-offset-1 text-[#0087E7]"
-          >
-            Sign Up
-          </Link>
-        </p>
       </div>
     </div>
   );
