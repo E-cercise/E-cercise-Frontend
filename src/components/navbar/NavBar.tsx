c0c8d680d422814955e8d87f738621fff070da1f
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2025-03-26 11:32:21 +0700

    add nav bar navigation

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -103,9 +103,9 @@ function NavBar({
     return (
         <>
             <div className="flex items-center bg-[#2D2A32] p-2 space-x-10 sticky top-0 z-[999]">
-                <Link to="/">
-                    <div className="flex items-center space-x-4 ml-4">
-                        <img src={ECerciseLogo} alt="E-Cercise Logo" className="h-14 ml-4"/>
+                <Link to="/" className="hover:opacity-80 transition-opacity">
+                    <div className="flex items-center space-x-4 ml-4 cursor-pointer">
+                        <img src={ECerciseLogo} alt="E-Cercise Logo" className="h-14 ml-4" />
                     </div>
                 </Link>
                 <Input.Search
